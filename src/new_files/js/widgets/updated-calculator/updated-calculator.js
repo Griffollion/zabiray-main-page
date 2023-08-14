c817fe7f4b13c8bb146090e1f01a45eb78719138
Denis Filippovich <frontend@ru.myfin.by>
2023-08-14 12:50:49 +0300

    make additional chart

--- src/new_files/js/widgets/updated-calculator/updated-calculator.js
+++ src/new_files/js/widgets/updated-calculator/updated-calculator.js
@@ -1,3 +1,54 @@
 (function () {
+    const is_mobile = !window.matchMedia('(min-width: 768px)').matches;
 
+    function initChart(tgt, data, options) {
+
+        window.myChart = new Chart(tgt, {
+            type: 'doughnut',
+            options: options,
+            data: data,
+        })
+
+    }
+
+    const chart = document.getElementById('chart')
+    const data = {
+        labels: [
+            'In interests',
+            'Towards the principal',
+        ],
+        datasets: [{
+            label: 'My First Dataset',
+            data: [300, 50],
+            backgroundColor: [
+                '#FF662B',
+                '#DDDDDD'
+            ],
+
+            hoverOffset: 4,
+            borderWidth: 4,
+            hoverBorderWidth: 4,
+            cutout: '88%'
+        }]
+    }
+
+    const config = {
+        type: 'doughnut',
+        data: data,
+        options: {
+            plugins: {
+                legend: {
+                    display: false
+                },
+                tooltip: {
+                    enabled: false,
+                    position: 'nearest',
+                }
+            },
+            borderColor: "transparent",
+            borderWidth:0
+        }
+    };
+
+    new Chart(chart, config)
 })()
